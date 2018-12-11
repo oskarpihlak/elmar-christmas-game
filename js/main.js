701c0ceb169bd95d6d61fc507dbdcb69e688ac7b
oskarpihlak <oskar.pihlak@gmail.com>
2018-12-11 22:42:47 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,12 +1,66 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbywQgZReY5g2N19B_BTeuWPDGFjHmWoXovyUvWNV4elXMotgFAS/exec';
-const boxesCount = 7;
-const winningBox = Math.floor(Math.random() * (boxesCount + 1));
 let selectedBoxes = [];
-let boxes = '';
 
 Number.prototype.pad = function (n) {
   return new Array(n).join('0').slice((n || 2) * -1) + this;
 };
+
+const giftData = [
+  {
+    title: 'Alkomeeter Alcoscan Secret',
+    titleUrl: '//pood.alkomeeter.com/',
+    subTitle: 'Turvalised sõidud tagab jõuluhinnaga Alcoscan Secret - vaata ruttu: alkomeeter.com',
+    image:'alko.jpg',
+    textUnderImage: 'Rovico ettevõtted said alguse juba 1992.aastal.Tänaseks oleme kasvanud Eesti suurimaks alkomeetrite müüjaks ja hooldajaks ning ainsaks tootjaks. Iga 5.autojuht Eestis kasutab meie alkomeetrit, mis tõstab Eesti alkomeetri kasutuse populaarsuselt esikohale kogu maailmas.'
+  },
+  {
+    title: 'Roosa Kvarts küünlaalus',
+    titleUrl: '//retreat4relief.com/',
+    subTitle: 'Roosa Kvarts on ülim armastusekristall, mis kannab endaga kaasas tundeid ja suhteid tervendavat väge. Kristallküünlaalus aitab perekonda ja koju tuua armastuseõnne. Väeese, mis aitab pereliikmetel omavahel hästi läbi saada ja parandada suhteid igal erineval tasandil.',
+    image:'kvarts.jpg',
+    textUnderImage: 'LA TENE on selgeltnägija Tene Laulu kristallipood ja portaal, kus leiad väga palju põnevat ning müstilist. Naturaalsed poolvääriskivid, viirukid, küünlad ja eeterlikud õlid on parimaks alternatiivteraapiaks sinu kehale ja hingele.'
+  },
+  {
+    title: '30€ Kaubamajaka kinkekaart',
+    titleUrl: '//kaubamajakas.ee/',
+    subTitle: 'Pärnu suurim ostukeskus Kaubamajakas on Pärnu lahe ääres asetsev kaubanduskeskus, kelle eesmärgiks on pakkuda lihtsat, kvaliteetset ja lõbusat poodlemiskogemust. \n' +
+      'Kaubamajakas ostlemine on eelkõige mugav just seetõttu, et käeulatuses võib leida mitmeid erinevaid kaupu. Mitmed erinevad tehnikakauplused, raamatupoed, ilu- ja tervisekaubad, jalanõud ja kotid, rõivad, lastekaubad, ehted, kellad ja kingad. Ausalt öeldes on Kaubamajakal miskit kõigile!',
+    image:'kaubamajakas.jpg',
+    textUnderImage: 'Kaubamajakasse on lihtne ligi pääseda. Kauplemispind asub esimesel korrusel ning on samal tasapinnal, mis parkla. Oled ostukäru, suure perekonna või on Sul hoopis liikumisraskused, siis meie juures ei pea tundma muret pikkade treppide või keerukate koridoride pärast.'
+  },
+];
+const giftBoxes = giftData.map((data, index) => {
+  return `
+    <div id="box-${index}" class="christmas-gift" onclick="openModal(${index})">
+      <img class="christmas-gift-element" src="img/${Math.floor(Math.random() * (5 - 1 + 1)) + 1}.png">
+    </div>
+    <div id="gift-${index}" class="elmar-christmas-form hidden">
+    <img class="elmar-christmas-form-logo" src="img/elmar.png" alt="">
+    <img class="elmar-christmas-form-exit" src="img/ristialus.png" alt="cross">
+    <img class="elmar-christmas-form-exit-btn" src="img/x.png" alt="cross-btn" onclick="closeModal(${index})">
+    <a class="elmar-christmas-form-title-link" target="_blank" href="${data.titleUrl}"><b class="elmar-christmas-form-title">${data.title}</b></a>
+    <div class="elmar-christmas-form-title-subtext">${data.subTitle}</div>
+    <div class="elmar-christmas-form-core">
+      <div class="elmar-christmas-form-core-prize">
+        <img class="elmar-christmas-form-core-img scale-down" src="img/gifts/${data.image}" alt="">
+      </div>
+      <form name="submit-to-google-sheet" id="person-form" class="elmar-christmas-form-core-fields">
+        <input id="personName" class="elmar-christmas-form-core-input" placeholder="Nimi" name="name">
+        <input id="personEmail" class="elmar-christmas-form-core-input" placeholder="e-post" name="email">
+        <div class="elmar-christmas-form-core-checkbox">
+          <input id="validateAgreement" class="elmar-christmas-form-core-checkbox-confirm" type="checkbox" name="checkbox" id="checkbox_id">
+          <label class="elmar-christmas-form-core-checkbox-text" for="validateAgreement">Nõustun <span class="elmar-christmas-form-core-checkbox-label">kampaania tingimustega</span></label>
+        </div>
+        <button type="submit" id="submit-form" class="elmar-christmas-form-submit">SAADA</button>
+      </form>
+    </div>
+    <div class="elmar-christmas-form-footer-text">
+      Kampaaniat korraldab AS Eesti Meedia, registreeritud asukoht Tallinn, Maakri 23a, registrikood 10184643, e-post reklaam@eestimeedia.ee, telefon +3726662350. <span class="terms-and-conditions-ref" onclick="openTermsAndConditions()">Kampaania reegleid näed siit</span>
+    </div>
+  </div>
+`
+});
+document.querySelector('.christmas-gift-container').innerHTML = giftBoxes;
 
 
 const form = document.forms['submit-to-google-sheet'];
@@ -30,42 +84,34 @@ form.addEventListener('submit', e => {
         <p class="elmar-christmas-form-sent-subtext">Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense!</p>
       `;
       })
-      .catch(() => {});
+      .catch(() => {
+      });
   } else {
     alert('Palun nõustuge kampaania tingimustega.');
     return false;
   }
 });
 
-function closeModal() {
-  document.querySelector('.elmar-christmas-form').classList.add('hidden');
-  document.querySelector('.elmar-christmas-heading').classList.remove('hidden');
-  document.querySelector('.christmas-gift-container').classList.remove('hidden');
+function closeModal(boxId) {
   document.querySelector('.elmar-christmas-heading-subtitle').classList.remove('hidden');
+  document.querySelector('.elmar-christmas-heading').classList.remove('hidden');
+  document.querySelector(`#gift-${boxId}`).classList.add('hidden');
+  document.querySelectorAll('.christmas-gift').forEach(element => element.classList.remove('hidden'))
 }
 
-function evaluateBox(boxId) {
-  console.log('triggered' + boxId);
-  if (boxId === winningBox) {
+function openModal(boxId) {
     document.querySelector(`#box-${boxId}`).innerHTML += `<img class="christmas-gift-element-status-checked" src="img/green-check.png" alt="d">`;
     setTimeout(() => {
       document.querySelector('.elmar-christmas-heading-subtitle').classList.add('hidden');
       document.querySelector('.elmar-christmas-heading').classList.add('hidden');
-      document.querySelector('.elmar-christmas-form').classList.remove('hidden');
-      document.querySelector('.christmas-gift-container').classList.add('hidden');
+      document.querySelector(`#gift-${boxId}`).classList.remove('hidden');
+      document.querySelectorAll('.christmas-gift').forEach(element => element.classList.add('hidden'))
     }, 1500);
-  } else {
-    if (!selectedBoxes.includes(boxId)) {
-      selectedBoxes.push(boxId);
-      document.querySelector(`#box-${boxId}`).innerHTML += `<img class="christmas-gift-element-status-crossed" src="img/red_x.png" alt="d">`;
-    }
-  }
 }
-
-for (let i = 0; i < boxesCount; i++) {
-  boxes += `
-      <div id="box-${i}" class="christmas-gift" onclick="evaluateBox(${i})">
-        <img class="christmas-gift-element" src="img/${Math.floor(Math.random() * (5 - 1 + 1)) + 1}.png">
-      </div>`;
+function openTermsAndConditions() {
+  document.querySelector(`.terms-and-conditions-container`).classList.remove('hidden');
 }
-document.querySelector('.christmas-gift-container').innerHTML = boxes;
+function closeTermsAndConditions() {
+  console.log('clse');
+  document.querySelector(`.terms-and-conditions-container`).classList.add('hidden');
+}
